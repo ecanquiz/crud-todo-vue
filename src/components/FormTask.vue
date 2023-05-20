a82235ef2cca6e75ed17396aeba7fed098184e8f
Ernesto Canquiz <fdsoil123@gmail.com>
2023-05-20 14:26:58 -0400

    feat: Refactorizar Vista CreateOrEdit.vue

--- src/components/FormTask.vue
+++ src/components/FormTask.vue
@@ -1,5 +1,44 @@
 <script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    task: Object    
+  },
+  data() {
+    return {
+      form: this.$props.task
+    }
+  },
+  emits: ['submit'],
+  methods: {
+    submit() {          
+      this.$emit('submit', this.form )
+    }
+  }
+})
 </script>
 
 <template>
+  <form @submit.prevent="submit">
+    <div class="m-2">
+      <label>Title</label>
+      <input type="text" v-model="form.title">
+    </div>
+
+   <div class="m-4">
+      <label>Description</label>
+      <textarea v-model="form.description"></textarea>
+    </div>
+
+    <div class="m-4">
+      <label>Done</label>
+      <input type="checkbox" v-model="form.done"/>
+    </div>
+
+    <button type="submit" class="btn btn-primary m-2">
+      Save
+    </button>
+  </form>
 </template>
+
